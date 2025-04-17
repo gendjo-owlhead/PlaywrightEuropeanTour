1127a188bed908bae5d02da2e611de18f7a575df
Gendzaj Mustafov <gendzaj.mustafov@example.com>
2025-04-17 10:39:21 +0200

    Add JUnit reporter for Jenkins test reporting

--- playwright.config.ts
+++ playwright.config.ts
@@ -10,7 +10,10 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: 'html',
+  reporter: [
+    ['html'],
+    ['junit', { outputFile: 'test-results/junit-results.xml' }]
+  ],
   use: {
     actionTimeout: 0,
     baseURL: 'https://www.europeantour.com',
